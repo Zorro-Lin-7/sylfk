c6fd8b17f115da4219a603a74f5fb5105b6557f8
linzhun <linzhun7@gmail.com>
2018-11-25 10:28:55 +0800

    实现__call__方法前，先实现WSGI入口

---
+++ sylfk/wsgi_adapter/__init__.py
@@ -0,0 +1,20 @@
+from werkzeug.wrappers import Request
+
+
+# WSGI 调度框架的入口
+def wsgi_app(app, environ, start_response):
+    """
+    第一个参数是应用，
+    第二个参数是服务器传过来的请求，
+    第三个参数是响应载体，这个参数我们完全不会用到，只需要连同处理结果一起传回给服务器就行
+    """
+
+    # 解析请求头（request header）
+    request = Request(environ)
+
+    # 把请求传给框架的路由进行处理，并获取处理结果
+    response = app.dispatch_request(request)
+
+    # 返回给服务器
+    return response(environ, start_response)
+
