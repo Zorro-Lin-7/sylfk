b9de4e23eae8edb5d4816950e4f999b2bd5718c8
linzhun <linzhun7@gmail.com>
2018-11-25 09:41:39 +0800

    建立框架主体

---
+++ sylfk/__init__.py
@@ -0,0 +1,19 @@
+class SYLFk:
+
+    # 实例化方法
+    def __init__(self):
+        pass
+
+    # 路由
+    def dispatch_request(self):
+        pass
+
+    # 启动入口
+    def run(self):
+        pass
+
+    # WSGI调用框架入口的方法
+    def __call__(self):
+        pass
+
+
