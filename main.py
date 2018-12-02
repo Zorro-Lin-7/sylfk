0aaf32ab3a42b21d558232da22b9cf07e61e9681
林准 <linzhun@linzhundeMacBook-Pro-2.local>
2018-12-02 11:08:55 +0800

    添加注释，详解路由装饰器的执行逻辑

--- main.py
+++ main.py
@@ -3,15 +3,39 @@ from sylfk import SYLFk
 
 app = SYLFk()
 
+'''
 @app.route('/index', methods=['GET'])
 def index():
     return '这是一个路由测试页面'
+'''
+# 等价于:
+
+def index():
+    return '这是一个路由测试页面'
+
+index = app.route('/index', methods=['GET'])(index)
+    # 等价于调用: .add_url_rule(url='index', f=index, 'route', methods=['GET'])
+
+#          ⬇等价于
+#       decorator(index)
+#             ⬇
+    # def decorator(f):
+        # # 调用应用内部的 add_url_rule 添加规则
+        # self.app.add_url_rule(url, f, 'route', **options) # [self.app] 已在上面定义，即应用本身
+        # return f
 
 
 @app.route('/test/js')
 def test_js():
     return '<script src="/static/test.js"></script>'
 
+# 等价于
+'''
+def test_js():
+    return '<script src="/static/test.js"></script>'
+
+test_js = app.route('/test/js')(test_js)
+'''
 
 app.run()
 
